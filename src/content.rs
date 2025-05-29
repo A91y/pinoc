ae7a1d223facf8ad6997c9da96e408b17335b445
4rjunc <arjunmundamani@gmail.com>
2025-05-30 01:22:58 +0530

    fix: all imports

--- src/content.rs
+++ src/content.rs
@@ -50,9 +50,9 @@ fn process_instruction(
         .ok_or(ProgramError::InvalidInstructionData)?;
 
     match ProgramInstruction::try_from(ix_disc)? {
-        ProgramInstruction::InitializeState => {
+        ProgramInstruction::Initialize => {
             msg!("Ix:0");
-            instruction::initilaize(accounts, instruction_data)
+            instructions::initilaize(accounts, instruction_data)
         }
     }
 }      "#
@@ -159,7 +159,7 @@ use crate::{
 };
 
 #[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, shank::ShankType)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Initialize {
     pub owner: Pubkey,
     pub bump: u8,
@@ -220,9 +220,9 @@ pub fn initilaize(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
         pub fn instructions_mod_rs() -> &'static str {
             r#"use pinocchio::program_error::ProgramError;
 
-pub mod initialize;
+pub mod initilaize;
 
-pub use initialize::*;
+pub use initilaize::*;
 
 #[repr(u8)]
 pub enum ProgramInstruction {
@@ -238,8 +238,7 @@ impl TryFrom<&u8> for ProgramInstruction {
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
-}
-            "#
+}"#
         }
     }
 
@@ -298,45 +297,72 @@ impl MyState {
         }
 
         pub fn utils_rs() -> &'static str {
-            r#"use super::utils::{load_acc_mut_unchecked, DataLen};
-use pinocchio::{
-    account_info::AccountInfo,
-    program_error::ProgramError,
-    pubkey::{self, Pubkey},
-    ProgramResult,
-};
+            r#"use pinocchio::program_error::ProgramError;
 
-use crate::{errors::MyProgramError, instructions::Initialize};
+use crate::errors::MyProgramError;
 
-#[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, shank::ShankAccount)]
-pub struct MyState {
-    pub owner: Pubkey,
+pub trait DataLen {
+    const LEN: usize;
 }
 
-impl DataLen for MyState {
-    const LEN: usize = core::mem::size_of::<MyState>();
+pub trait Initialized {
+    fn is_initialized(&self) -> bool;
 }
 
-impl MyState {
-    pub const SEED: &'static str = "init";
-
-    pub fn validate_pda(bump: u8, pda: &Pubkey, owner: &Pubkey) -> Result<(), ProgramError> {
-        let seed_with_bump = &[Self::SEED.as_bytes(), owner, &[bump]];
-        let derived = pubkey::create_program_address(seed_with_bump, &crate::ID)?;
-        if derived != *pda {
-            return Err(MyProgramError::PdaMismatch.into());
+#[inline(always)]
+pub unsafe fn load_acc<T: DataLen + Initialized>(bytes: &[u8]) -> Result<&T, ProgramError> {
+    load_acc_unchecked::<T>(bytes).and_then(|acc| {
+        if acc.is_initialized() {
+            Ok(acc)
+        } else {
+            Err(ProgramError::UninitializedAccount)
         }
-        Ok(())
-    }
+    })
+}
 
-    pub fn initialize(my_stata_acc: &AccountInfo, ix_data: &Initialize) -> ProgramResult {
-        let my_state =
-            unsafe { load_acc_mut_unchecked::<MyState>(my_stata_acc.borrow_mut_data_unchecked()) }?;
-
-        my_state.owner = ix_data.owner;
-        Ok(())
+#[inline(always)]
+pub unsafe fn load_acc_unchecked<T: DataLen>(bytes: &[u8]) -> Result<&T, ProgramError> {
+    if bytes.len() != T::LEN {
+        return Err(ProgramError::InvalidAccountData);
     }
+    Ok(&*(bytes.as_ptr() as *const T))
+}
+
+#[inline(always)]
+pub unsafe fn load_acc_mut<T: DataLen + Initialized>(
+    bytes: &mut [u8],
+) -> Result<&mut T, ProgramError> {
+    load_acc_mut_unchecked::<T>(bytes).and_then(|acc| {
+        if acc.is_initialized() {
+            Ok(acc)
+        } else {
+            Err(ProgramError::UninitializedAccount)
+        }
+    })
+}
+
+#[inline(always)]
+pub unsafe fn load_acc_mut_unchecked<T: DataLen>(bytes: &mut [u8]) -> Result<&mut T, ProgramError> {
+    if bytes.len() != T::LEN {
+        return Err(ProgramError::InvalidAccountData);
+    }
+    Ok(&mut *(bytes.as_mut_ptr() as *mut T))
+}
+
+#[inline(always)]
+pub unsafe fn load_ix_data<T: DataLen>(bytes: &[u8]) -> Result<&T, ProgramError> {
+    if bytes.len() != T::LEN {
+        return Err(MyProgramError::InvalidInstructionData.into());
+    }
+    Ok(&*(bytes.as_ptr() as *const T))
+}
+
+pub unsafe fn to_bytes<T: DataLen>(data: &T) -> &[u8] {
+    core::slice::from_raw_parts(data as *const T as *const u8, T::LEN)
+}
+
+pub unsafe fn to_mut_bytes<T: DataLen>(data: &mut T) -> &mut [u8] {
+    core::slice::from_raw_parts_mut(data as *mut T as *mut u8, T::LEN)
 }"#
         }
     }
